fc7826633d94f52e7d808b798a4cc4efb45c9781
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2021-10-20 00:00:25 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <button className='bg-green-500 hover:border-gray-300'>Button</button>
+      <div className='bg-purple-600 md:bg-opacity-50 hover:bg-red-700'>
+        Some new Button
+      </div>
+
+      <div className='App'>Hello</div>
+    </>
+  );
+}
+
+export default App;
